a04cade404607ad947cd99d0373c25e3ac122fc9
huangfu <huangfusuper@163.com>
2018-12-25 01:46:02 +0800

    Spring Cloud 1.0 开发了Spring Cloud Eureka注册中心

---
+++ springcloud_eureka/src/main/java/com/baizhi/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 皇甫
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
